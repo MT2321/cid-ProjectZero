4eec5f39b5eeacce7e80a604b4bafae31d52b44d
pablosml <psmolkin@itba.edu.ar>
2019-09-06 16:43:57 -0300

    cambios a la funcion getVector

--- tempClass.py
+++ tempClass.py
@@ -10,17 +10,30 @@
 - list(year-1) getEvolByStep(w1)
 '''
 
+import numpy as np
+from scipy import spatial
+
 class tempName:
-    def __init__(self, matrixes, vocabulary):
-        self.matrixes = matrixes
-        self.vocabulary = vocabulary
+    def __init__(self, matrixes, yearDict, vocabulary):
+        self.matrixes = matrixes    # matrices de probabilidad de coocurrencia por year
+        self.yearDict = yearDict    # diccionario que vincula year con indice en matrixes
+        self.vocabulary = vocabulary # vocabulario utilizado para generar las matrices
+        self.inverseVocab = dict(map(reversed, vocabulary.items())) # diccionario inverso del vocabulario
 
     def findSimilar(self, vector, threshold, year):
+        tempMat = self.matrixes[self.yearDict[year]] # obtengo la matriz del anio pedido
+        results = [] # container para los resultados encontrados (strings)
+        # np.transpose(tempMat) si la palabra es la columna
         if type(threshold) is float:
-            pass
-            # se busca similitud coseno
+            for index, word in enumerate(tempMat): # ASUMIENDO QUE LA PALABRA ES LA FILA!!!
+                cosSim = 1 - spatial.distance.cosine(vector, word) # se obtiene la similitud coseno entre word y vector
+                if cosSim > threshold: # si la similitud coseno cae dentro del threshold dado
+                    results.append(self.inverseVocab[index]) # se guarda en results la palabra encontrada
         elif type(threshold) is int:
-            pass
+            similarities = []
+            for index, word in enumerate(tempMat): # ASUMIENDO QUE LA PALABRA ES LA FILA!!!
+                cosSim = 1 - spatial.distance.cosine(vector, word) # se obtiene la similitud coseno entre word y vector
+                similarities.append([index, cosSim])
             # se busca por cantidad
         
         resultados = {} # diccionario con las palabras encontradas y su sim-cos respecto de la ingresada
@@ -43,11 +56,11 @@ class tempName:
         return vector
 
     def getSim(self, w1, y1, w2, y2):
-        return cosSim
-    
+        #return cosSim
+        pass
     def getEvol(self, w1, y1, w2):
-        return evol
-
+        #return evol
+        pass
     def getEvolByStep(self, word):
         evolution = []
 
