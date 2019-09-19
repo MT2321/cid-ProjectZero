181d4d371f8a851605a9eda6f86dc21acad641c0
MT2321 <mrodriguez@itba.edu.ar>
2019-09-19 01:43:32 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('C:/Users/RA/Documents/GitHub/cid-ProjectZero/paquete'))
-sys.path.insert(0, os.path.abspath('classPackg\SimiLab.py'))
+sys.path.insert(0, os.path.abspath('../classPackg/SimiLab.py'))
 
 # -- Project information -----------------------------------------------------
 
